d3372fff778fa8a271b07c2a68c37d7ffff8c003
Cape <raulcape.7@gmail.com>
2018-12-01 18:47:24 +0100

    Dibujado el rectangulo para la cara

--- faceDetection.py
+++ faceDetection.py
@@ -13,4 +13,13 @@ def detect(gray, imagen):
     #1.3 factor de reduccion de la imagen
     #5 zonas deben ser aceptadas
     faces = cara_cascade.detectMultiScale(gray, 1.3, 5)
-    
+    #Coordenadas desde arriba a la izquierda x,y
+    #W ancho del rectangulo
+    #H altura del rectangulo
+    for(x,y,w,h) in faces: 
+        #Rectangle es una funcion dibuja el rectangulo
+        #El tercer argumento son las coordenadas de la parte inferior izquierda
+        #del rectangulo
+        #2 es el ancho de la linea del rectangulo
+        cv2.rectangle(frame,(x,y),(x+w, y+h), (255,0,0), 2)
+        
