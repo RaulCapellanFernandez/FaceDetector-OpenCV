ebad7f28bc869b275177feadc2fb13b21ea21a56
Cape <raulcape.7@gmail.com>
2018-12-01 18:39:03 +0100

    Creada la variable en cargada de la deteccion de la cara

--- faceDetection.py
+++ faceDetection.py
@@ -1,8 +1,16 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Dec  1 18:21:01 2018
+#Face Detection
 
-@author: cp
-"""
+#Librerias de OpenCV
+import cv2
 
+#Carga los filtros creados en el XML
+cara_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+ojo_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+
+#Deteccion
+def detect(gray, imagen):
+    #La imagen original y la imagen en blanco y negro
+    #1.3 factor de reduccion de la imagen
+    #5 zonas deben ser aceptadas
+    faces = cara_cascade.detectMultiScale(gray, 1.3, 5)
+    
